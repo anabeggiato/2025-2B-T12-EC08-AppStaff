104369d4eb0fadc639a9d46c0948fd095acdffb9
Ana Beatriz Passos Beggiato <ana.beggiato@sou.inteli.edu.br>
2025-12-01 15:00:11 -0300

    feat: add DateTimePicker for tour scheduling in AddTourPopup

--- app/components/AddTourPopup.tsx
+++ app/components/AddTourPopup.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { StyleSheet, View, Text, Pressable, TextInput, ScrollView } from 'react-native'
+import { StyleSheet, View, Text, Pressable, TextInput, ScrollView, TouchableOpacity, Platform } from 'react-native'
 import { CheckBox } from 'react-native-elements'
 import Feather from '@expo/vector-icons/Feather'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import type { Tour } from '@/app/(tabs)/index'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 type Props = {
   onClose: () => void;
@@ -11,21 +12,34 @@ type Props = {
 };
 
 export function AddTourPopup({ onClose, addTour }: Props) {
-  const [responsavel, setResponsavel] = useState("");
-  const [data, setData] = useState("");
-  const [horaInicioPrevista, setHoraInicioPrevista] = useState("");
-  const [horaFimPrevista, setHoraFimPrevista] = useState("");
-  const [status, setStatus] = useState("A começar");
-  const [nomeVisitante, setNomeVisitante] = useState("");
-  const [emailVisitante, setEmailVisitante] = useState("");
-  const [perfilvisitante, setPerfilVisitante] = useState("");
-  const [estado, setEstado] = useState("");
-  const [cpf, setCpf] = useState("");
-  const [telefone, setTelefone] = useState("");
-  const [cidade, setCidade] = useState("");
-  const [acompanhante, setAcompanhante] = useState(false);
-  const [nomeAcompanhante, setNomeAcompanhante] = useState("");
-  const [cpfAcompanhante, setCpfAcompanhante] = useState("")
+
+  const [form, setForm] = useState({
+    responsavel: "",
+    data: new Date(),
+    horaInicioPrevista: "",
+    horaFimPrevista: "",
+    status: "A começar",
+    nomeVisitante: "",
+    emailVisitante: "",
+    perfilvisitante: "",
+    estado: "",
+    cpf: "",
+    telefone: "",
+    cidade: "",
+    acompanhante: false,
+    nomeAcompanhante: "",
+    cpfAcompanhante: ""
+  });
+
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [showTimeInicio, setShowTimeInicio] = useState(false);
+  const [showTimeFim, setShowTimeFim] = useState(false);
+
+
+  function updateField(field, value) {
+    setForm(prev => ({ ...prev, [field]: value }));
+  }
 
   function generateCode() {
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -35,7 +49,6 @@ export function AddTourPopup({ onClose, addTour }: Props) {
     for (let i = 0; i < 4; i++) {
       const randomLetter = letters[Math.floor(Math.random() * letters.length)];
       const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-
       code += randomLetter + randomNumber;
     }
 
@@ -45,97 +58,178 @@ export function AddTourPopup({ onClose, addTour }: Props) {
   function handleSubmit() {
     const newTour: Tour = {
       codigo: generateCode(),
-      responsavel: responsavel,
+      responsavel: form.responsavel,
       status: "scheduled",
-      data: data,
-      hora_inicio_prevista: horaInicioPrevista,
-      hora_fim_prevista: horaFimPrevista
+      data: form.data,
+      hora_inicio_prevista: form.horaInicioPrevista,
+      hora_fim_prevista: form.horaFimPrevista
     };
 
     addTour(newTour);
     onClose();
   }
 
+  const showDatepicker = () => {
+    setShow(true);
+  };
+
+  const changeDate = (event, selectedDate) => {
+    const currentDate = selectedDate || form.data;
+    setShow(Platform.OS === 'ios');
+    updateField("data", currentDate);
+  };
+
+  const formatarData = (d) => {
+    return d.toLocaleDateString("pt-BR");
+  };
+
+  const formatarHora = (date: Date) => {
+    return date.toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false
+    });
+  };
+
+  const changeHoraInicio = (event, selectedDate) => {
+    if (selectedDate) {
+      // Define horário inicial
+      updateField("horaInicioPrevista", formatarHora(selectedDate));
+
+      // Cria nova data para o horário final
+      const fim = new Date(selectedDate.getTime());
+      fim.setHours(fim.getHours() + 1);
+
+      // Define horário final automaticamente
+      updateField("horaFimPrevista", formatarHora(fim));
+    }
+
+    setShowTimeInicio(false);
+  };
+
+
+  const changeHoraFim = (event, selectedDate) => {
+    if (selectedDate) {
+      updateField("horaFimPrevista", formatarHora(selectedDate));
+    }
+    setShowTimeFim(false);
+  };
+
+
   return (
     <View style={styles.overlay}>
       <View style={styles.add_tour_popup}>
-        <View style={[styles.topo,]}>
+        <View style={[styles.topo]}>
           <Text style={styles.title}>Cadastrar novo tour</Text>
           <View style={styles.botoes}>
             <Pressable onPress={handleSubmit}>
               <Feather name="check-circle" size={20} color="#9747FF" />
             </Pressable>
 
-            <Pressable onPress={() => { onClose() }}>
+            <Pressable onPress={onClose}>
               <MaterialIcons name="close" size={20} color="black" />
             </Pressable>
           </View>
         </View>
+
         <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
 
-          {/*Informações gerais do tour */}
+          {/*Informações gerais*/}
           <View style={styles.bloco_input}>
             <View style={[styles.input_section, { width: "48%" }]}>
               <Text style={styles.label}>Staff</Text>
               <TextInput
                 style={styles.input}
                 editable
-                onChangeText={text => setResponsavel(text)}
-                value={responsavel}
+                onChangeText={text => updateField("responsavel", text)}
+                value={form.responsavel}
               />
             </View>
 
             <View style={[styles.input_section, { width: "48%" }]}>
               <Text style={styles.label}>Data</Text>
-              <TextInput
-                style={styles.input}
-                editable
-                onChangeText={text => setData(text)}
-                value={data}
-              />
+
+              <TouchableOpacity onPress={showDatepicker}>
+                <TextInput
+                  style={styles.input}
+                  value={formatarData(form.data)}
+                  editable={false}
+                  pointerEvents="none"
+                />
+              </TouchableOpacity>
+
+              {show && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={form.data}
+                  mode="date"
+                  is24Hour={true}
+                  display="default"
+                  onChange={changeDate}
+                />
+              )}
             </View>
 
             <View style={[styles.input_section, { width: "48%" }]}>
               <Text style={styles.label}>Horário inicial</Text>
-              <TextInput
-                style={styles.input}
-                editable
-                onChangeText={text => setHoraInicioPrevista(text)}
-                value={horaInicioPrevista}
-              />
+
+              <TouchableOpacity onPress={() => setShowTimeInicio(true)}>
+                <TextInput
+                  style={styles.input}
+                  value={form.horaInicioPrevista}
+                  editable={false}
+                  pointerEvents="none"
+                />
+              </TouchableOpacity>
+
+              {showTimeInicio && (
+                <DateTimePicker
+                  testID="timePickerInicio"
+                  value={new Date()}
+                  mode="time"
+                  is24Hour={true}
+                  display="default"
+                  onChange={changeHoraInicio}
+                />
+              )}
             </View>
+
 
             <View style={[styles.input_section, { width: "48%" }]}>
               <Text style={styles.label}>Horário final</Text>
-              <TextInput
-                style={styles.input}
-                editable
-                onChangeText={text => setHoraFimPrevista(text)}
-                value={horaFimPrevista}
-              />
-            </View>
 
-            <View style={[styles.input_section, { width: "48%" }]}>
-              <Text style={styles.label}>Status</Text>
-              <TextInput
-                style={styles.input}
-                editable
-                onChangeText={text => setStatus(text)}
-                value={status}
-              />
+              <TouchableOpacity onPress={() => setShowTimeFim(true)}>
+                <TextInput
+                  style={styles.input}
+                  value={form.horaFimPrevista}
+                  editable={false}
+                  pointerEvents="none"
+                />
+              </TouchableOpacity>
+
+              {showTimeFim && (
+                <DateTimePicker
+                  testID="timePickerFim"
+                  value={new Date()}
+                  mode="time"
+                  is24Hour={true}
+                  display="default"
+                  onChange={changeHoraFim}
+                />
+              )}
             </View>
           </View>
 
-          {/*informações do visitante*/}
+          {/*Visitantes*/}
           <Text style={[styles.title, { paddingBottom: 8 }]}>Visitantes</Text>
+
           <View style={styles.bloco_input}>
             <View style={[styles.input_section, { width: "95%" }]}>
               <Text style={styles.label}>Nome</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setNomeVisitante(text)}
-                value={nomeVisitante}
+                onChangeText={text => updateField("nomeVisitante", text)}
+                value={form.nomeVisitante}
               />
             </View>
 
@@ -143,9 +237,8 @@ export function AddTourPopup({ onClose, addTour }: Props) {
               <Text style={styles.label}>E-mail</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setEmailVisitante(text)}
-                value={emailVisitante}
+                onChangeText={text => updateField("emailVisitante", text)}
+                value={form.emailVisitante}
               />
             </View>
 
@@ -153,9 +246,8 @@ export function AddTourPopup({ onClose, addTour }: Props) {
               <Text style={styles.label}>Perfil</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setPerfilVisitante(text)}
-                value={perfilvisitante}
+                onChangeText={text => updateField("perfilvisitante", text)}
+                value={form.perfilvisitante}
               />
             </View>
 
@@ -163,9 +255,8 @@ export function AddTourPopup({ onClose, addTour }: Props) {
               <Text style={styles.label}>CPF</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setCpf(text)}
-                value={cpf}
+                onChangeText={text => updateField("cpf", text)}
+                value={form.cpf}
               />
             </View>
 
@@ -173,9 +264,8 @@ export function AddTourPopup({ onClose, addTour }: Props) {
               <Text style={styles.label}>Telefone</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setTelefone(text)}
-                value={telefone}
+                onChangeText={text => updateField("telefone", text)}
+                value={form.telefone}
               />
             </View>
 
@@ -183,9 +273,8 @@ export function AddTourPopup({ onClose, addTour }: Props) {
               <Text style={styles.label}>Estado</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setEstado(text)}
-                value={estado}
+                onChangeText={text => updateField("estado", text)}
+                value={form.estado}
               />
             </View>
 
@@ -193,9 +282,8 @@ export function AddTourPopup({ onClose, addTour }: Props) {
               <Text style={styles.label}>Cidade</Text>
               <TextInput
                 style={styles.input}
-                editable
-                onChangeText={text => setCidade(text)}
-                value={cidade}
+                onChangeText={text => updateField("cidade", text)}
+                value={form.cidade}
               />
             </View>
           </View>
@@ -203,18 +291,18 @@ export function AddTourPopup({ onClose, addTour }: Props) {
           <View>
             <CheckBox
               title='Vai trazer acompanhante?'
-              checked={acompanhante}
-              onPress={() => setAcompanhante(!acompanhante)} />
+              checked={form.acompanhante}
+              onPress={() => updateField("acompanhante", !form.acompanhante)}
+            />
 
-            {acompanhante && (
+            {form.acompanhante && (
               <>
                 <View style={[styles.input_section, { width: "95%" }]}>
                   <Text style={styles.label}>Nome do Acompanhante</Text>
                   <TextInput
                     style={styles.input}
-                    editable
-                    onChangeText={text => setNomeAcompanhante(text)}
-                    value={nomeAcompanhante}
+                    onChangeText={text => updateField("nomeAcompanhante", text)}
+                    value={form.nomeAcompanhante}
                   />
                 </View>
 
@@ -222,19 +310,20 @@ export function AddTourPopup({ onClose, addTour }: Props) {
                   <Text style={styles.label}>CPF do Acompanhante</Text>
                   <TextInput
                     style={styles.input}
-                    editable
-                    onChangeText={text => setCpfAcompanhante(text)}
-                    value={cpfAcompanhante}
+                    onChangeText={text => updateField("cpfAcompanhante", text)}
+                    value={form.cpfAcompanhante}
                   />
                 </View>
               </>
             )}
           </View>
+
         </ScrollView>
       </View>
     </View>
-  )
+  );
 }
+
 
 const styles = StyleSheet.create({
   overlay: {
